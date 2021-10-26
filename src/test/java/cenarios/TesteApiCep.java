01d2bfc71838182bdd9863b3f2e6e65ee2da93ca
qaacademy <qaacademy@gmail.com>
2021-10-26 11:46:12 -0300

    Testes do CEP - Correção

--- src/test/java/cenarios/TesteApiCep.java
+++ src/test/java/cenarios/TesteApiCep.java
@@ -1,40 +1,30 @@
 package cenarios;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Assert;
 import org.junit.Test;
 
 import io.restassured.RestAssured;
-import io.restassured.RestAssured.*;
+import  io.restassured.RestAssured.*;
+
+import org.hamcrest.Matchers;
+import  org.hamcrest.Matchers.*;
 
 public class TesteApiCep {
-	
+
 	@Test
 	public void testeApiCorreios() {
-		RestAssured.given().log().all()//pre condição
-		.when() //Disparo
-		.get("https://viacep.com.br/ws/05794400/json/") //endereço
-		.then().log().all() //Validação
-		.statusCode(200); // codigo retornado
-	
+		RestAssured.given().log().all()// pre condição
+				.when() // Disparo
+				.get("https://viacep.com.br/ws/05794400/json/") // endereço
+				.then().log().all() // Validação
+				.statusCode(200); // codigo retornado
+
 	}
-	
+
 	@Test
 	public void testeApiCorreiosBody() {
-		RestAssured.given().log().all()
-		.when()
-		.get("https://viacep.com.br/ws/05794400/json/") 
-		.then().log().all() 
-		.statusCode(200)
-		.assertThat()
-		.body(equalTo("05794-400"));
-		
-	
+		RestAssured.given().log().all().when().get("https://viacep.com.br/ws/05794400/json/").then().log().all().statusCode(200)
+				.assertThat().body("cep", Matchers.equalTo("05794-400"));
+
 	}
-	
-	
-	
-	
 
 }
